dd389f7a4e06cbdbb57b011900a0edb47f6e1afc
kourosh <kourosh23@hotmail.com>
2024-12-19 18:54:43 +0100

    ApplicationDbContext

---
+++ SportsStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+namespace SportsStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
