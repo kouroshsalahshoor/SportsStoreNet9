eb1c4837874e409937d56a10159ab5a3cc0a764d
kourosh <kourosh23@hotmail.com>
2024-12-20 08:08:23 +0100

    page count

--- SportsStore/Controllers/HomeController.cs
+++ SportsStore/Controllers/HomeController.cs
@@ -24,23 +24,25 @@ namespace SportsStore.Controllers
         //}
 
         public ViewResult Index(string? category, int productPage = 1)
-            => View(new ProductsListViewModel
-            {
-                Products = repository.Products
-            .Where(p => category == null ||
-            p.Category == category)
-            .OrderBy(p => p.ProductID)
-            .Skip((productPage - 1) * PageSize)
-            .Take(PageSize),
-                PagingInfo = new PagingInfo
-                {
-                    CurrentPage = productPage,
-                    ItemsPerPage = PageSize,
-                    TotalItems = repository.Products.Count()
-                },
-                CurrentCategory = category
-            });
-
+=> View(new ProductsListViewModel
+{
+    Products = repository.Products
+.Where(p => category == null
+|| p.Category == category)
+.OrderBy(p => p.ProductID)
+.Skip((productPage - 1) * PageSize)
+.Take(PageSize),
+    PagingInfo = new PagingInfo
+    {
+        CurrentPage = productPage,
+        ItemsPerPage = PageSize,
+        TotalItems = category == null
+? repository.Products.Count()
+: repository.Products.Where(e =>
+e.Category == category).Count()
+    },
+    CurrentCategory = category
+});
 
         public IActionResult Privacy()
         {
