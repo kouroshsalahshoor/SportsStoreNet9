6ec77b49c1d74cf1457b3cb33fe7a8061ed539e7
kourosh <kourosh23@hotmail.com>
2024-12-20 19:36:07 +0100

    products admin

--- SportsStore/Repository/IRepository/IStoreRepository.cs
+++ SportsStore/Repository/IRepository/IStoreRepository.cs
@@ -5,5 +5,9 @@ namespace SportsStore.Repository.IRepository
     public interface IStoreRepository
     {
         IQueryable<Product> Products { get; }
+        void SaveProduct(Product p);
+        void CreateProduct(Product p);
+        void DeleteProduct(Product p);
+
     }
 }
